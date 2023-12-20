953740e5d9f41a098e9b1ca71db6d8e57883a841
Your Tanmay <00tanmay@gmail.com>
2023-12-20 22:30:56 +0530

    added ai code explanation function using open_ai api

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,57 @@
 #! /usr/bin/env node
-console.log("Hello World!");
+
+const fs = require('fs');
+const path = require('path');
+const yargs = require('yargs');
+const OpenAI = require('openai');
+require('isomorphic-fetch');
+
+// Load environment variables from .env file
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
+const argv = yargs
+  .usage('Usage: cex [options] <file-name>')
+  .option('help', {
+    alias: 'h',
+    description: 'Show help',
+  })
+  .demandCommand(1, 'Please provide a file name or the file path.')
+  .argv;
+
+if (argv.help) {
+  yargs.showHelp();
+  process.exit(0);
+}
+
+const fileName = argv._[0];
+const filePath = argv.dir ? path.resolve(process.cwd(), fileName) : fileName;
+
+const apiKey = process.env.OPENAI_API_KEY;
+if (!apiKey) {
+  console.error('OpenAI API key not found. Make sure to set it in the .env file.');
+  process.exit(1);
+}
+const openai = new OpenAI({ apiKey });
+async function main() {
+  const code = fs.readFileSync(filePath, 'utf8');
+      // console.log('Code from the file:');
+      // console.log(code);
+      console.log("The explaination of the code is being loaded...\n")
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You are an experienced software developer.",
+      },
+      {
+        role: "user",
+        content: `Explain the following code:\n\n${code}`,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0].message.content);
+}
+
+main();
